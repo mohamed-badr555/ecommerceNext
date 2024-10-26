6319a4dad738afd1737bcfab96ea1cf590a2a499
mohamed-badr555 <mohamedb.555dr@gmail.com>
2024-10-27 00:29:49 +0300

    Upload App

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static.wixstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "people.pic1.co",
+      },
+      {
+        protocol: "https",
+        hostname: "app-uploads-cdn.fera.ai",
+      },
+    ],
+    // unoptimized: true, // This is required for static export
+  },
+  output: 'export', // Static export mode
+
+};
+
+export default nextConfig;
